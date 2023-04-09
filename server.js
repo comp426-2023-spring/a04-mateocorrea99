0ddb946d46e262b1873bc114ffba2a724d3f625f
mateocorrea99 <mcorrear@ad.unc.edu>
2023-04-09 16:30:08 -0400

    a04 fixed server.js

--- server.js
+++ server.js
@@ -12,42 +12,57 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.get('/app', (req, res) => {
-    res.status(200).send('200 OK').end();
+app.get("/app/", (req, res, next) => {
+    res.json({"message":"200 OK"});
+	res.status(200);
 });
 
-app.get('/app/rps', (req, res) => {
-    res.status(200).send(JSON.stringify(rps(req.body.shot))).end();
-})
+app.get("/app/rps", (req, res, next) => {
+    res.json(playRPS());
+	res.status(200);
+});
 
-app.get('/app/rpsls', (req, res) => {
-    res.status(200).send(JSON.stringify(rpsls(req.body.shot))).end();
-})
-app.get('/app/rps/play', (req, res) => {
-    res.status(200).send(JSON.stringify(rps(req.query.shot))).end();
-})
-app.get('/app/rpsls/play', (req, res) => {
-    res.status(200).send(JSON.stringify(rpsls(req.query.shot))).end();
-})
-app.post('/app/rps/play', (req, res) => {
-    res.status(200).send(JSON.stringify(rps(req.body.shot))).end();
-})
-app.post('/app/rpsls/play', (req, res) => {
-    res.status(200).send(JSON.stringify(rpsls(req.body.shot))).end();
-})
+app.get("/app/rpsls", (req, res, next) => {
+    res.json(playRPSLS());
+	res.status(200);
+});
 
-app.get('/app/rps/play/:shot', (req, res) => {
-    res.status(200).send(JSON.stringify(rps(req.params.shot))).end();
-})
+app.get("/app/rps/play", (req, res, next) => {
+    res.json(rps(req.query.shot));
+	res.status(200);
+});
 
-app.get('/app/rpsls/play/:shot', (req, res) => {
-    res.status(200).send(JSON.stringify(rpsls(req.params.shot))).end();
-})
 
-app.all('*', (req, res) => {
-    res.status(404).send('404 NOT FOUND').end();
-})
+app.get("/app/rpsls/play", (req, res, next) => {
+    res.json(playRPSLS(req.query.shot));
+	res.status(200);
+});
 
-app.listen(port, () => {
-    console.log(`Port ${port} working.`)
-})
+
+app.post("/app/rps/play", (req, res, next) => {
+    res.json(rps(req.body.shot));
+	res.status(200);
+});
+
+
+app.post("/app/rpsls/play", (req, res, next) => {
+    res.json(playRPSLS(req.body.shot));
+	res.status(200);
+});
+
+app.get("/app/rps/play/:shot", (req, res, next) => {
+    res.json(playRPS(req.params['shot']));
+	res.status(200);
+});
+
+
+app.get("/app/rpsls/play/:shot", (req, res, next) => {
+    res.json(playRPSLS(req.params.shot));
+	res.status(200);
+});
+
+
+app.use(function(req, res){
+	res.json({"message":"404 NOT FOUND"});
+    res.status(404);
+});
